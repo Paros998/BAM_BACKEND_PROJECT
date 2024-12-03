73308393adc3a9d1372622cb259ea457b62c7af4
Patryk Grzywacz <patrykg198@gmail.com>
2024-12-03 19:16:11 +0100

    fixed response status

--- src/main/java/psk/bam/jwt/JwtTokenFilter.java
+++ src/main/java/psk/bam/jwt/JwtTokenFilter.java
@@ -1,23 +1,5 @@
 package psk.bam.jwt;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import org.assertj.core.util.Strings;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Service;
-import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.server.ResponseStatusException;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jws;
@@ -29,7 +11,25 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.assertj.core.util.Strings;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class JwtTokenFilter extends OncePerRequestFilter {
@@ -67,7 +67,8 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
             } catch (final Exception e) {
                 if (e.getClass().equals(ExpiredJwtException.class)) {
-                    throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+                    log.error(e.getMessage());
+                    response.setStatus(HttpStatus.FORBIDDEN.value());
                 } else {
                     throw new IllegalStateException(String.format("Token %s cannot be trusted", token));
                 }
